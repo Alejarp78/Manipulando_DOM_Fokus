e15c948f50406e25d1b9b758e77aa026f693f46c
Jhozmer Ramírez <jhozmeraramirez@gmail.com>
2024-08-06 23:47:07 -0400

    Clase 04 completada - Configuración de Iniciar, pausa y de los tiempos de duración

--- 2049-FokusCurso-proyecto-inicial/script.js
+++ 2049-FokusCurso-proyecto-inicial/script.js
@@ -7,6 +7,13 @@ const titulo = document.querySelector('.app__title');
 const botones = document.querySelectorAll('.app__card-button');
 const inputEnfoqueMusica = document.querySelector('#alternar-musica');
 const musica = new Audio('./sonidos/luna-rise-part-one.mp3');
+const botonIniciarPausar = document.querySelector('#start-pause');
+const audioPlay = new Audio('./sonidos/play.wav');
+const audioPausa = new Audio('./sonidos/pause.mp3');
+const audioTiempoFinalizado = new Audio('./sonidos/beep.mp3');
+
+let tiempoTRanscurridoEnSegundos = 5;
+let idIntervalo = null;
 
 musica.loop = true;
 
@@ -61,3 +68,35 @@ function cambiarContexto(contexto) {
     };
 };
 
+const cuentaRegresiva = () => {
+    //iniciarPausar();
+    if (tiempoTRanscurridoEnSegundos <= 0) {
+        audioTiempoFinalizado .play()
+        //alert('Tiempo final');
+        console.log('Tiempo final');
+        reiniciar();
+        return // retorno anticipado -- circuit breaker
+    };
+
+    tiempoTRanscurridoEnSegundos -= 1;
+    console.log("Temporizador: " + tiempoTRanscurridoEnSegundos); // Muestra el tiempo actual
+    console.log('Id: ' + idIntervalo) // Muestra el ID actual
+};
+
+botonIniciarPausar.addEventListener('click', iniciarPausar);
+
+function iniciarPausar() {
+    if (idIntervalo) {
+        audioPausa.play();
+        reiniciar();
+        return // retorno anticipado -- circuit breaker
+    };
+
+    audioPlay.play();
+    idIntervalo = setInterval(cuentaRegresiva, 1000);
+};
+
+function reiniciar() {
+    clearInterval(idIntervalo);
+    idIntervalo = null;
+};
